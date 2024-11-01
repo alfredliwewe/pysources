61140662a3aff8d00b22d799f1ab1931d76fe7b2
alfredliwewe <liwewerobati@gmail.com>
2024-11-01 17:23:02 +0200

    Poyamba

---
+++ read_from_input.py
@@ -0,0 +1,9 @@
+# ask for name
+age = 18 #int
+name = input("Whats your name")
+
+# statement = "Welcome " + name + ", thank you "  # string adding / concatenation
+
+statement = "Welcome " + name + ", your age is " + str(age)
+
+print(statement)
---
+++ sample.py
@@ -0,0 +1,16 @@
+a = 10 # this is int
+b = 20
+
+pi = 3.143548954965 # double/float
+
+# we are adding a and b to get c 
+c = a + b
+
+firstName = "Alfred" # string
+lastName = "Liwewe"
+
+isMale = False # boolean
+
+fullName = firstName + " Sitolo"
+
+print(fullName)
